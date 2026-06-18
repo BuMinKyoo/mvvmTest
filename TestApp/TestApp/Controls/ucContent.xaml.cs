d615ef31302a8a219d8c02cec7d3676670155a15
mgbu <mgbu@clamos.io>
2026-06-18 10:44:26 +0900

    test할 코드 작성

--- TestApp/TestApp/Controls/ucContent.xaml.cs
+++ TestApp/TestApp/Controls/ucContent.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using TestApp.ViewModels;
@@ -9,9 +10,9 @@ namespace TestApp.Controls
     /// 순수 code-behind 카운터. 단, 데이터는 MVVM 화면(ucMvvmCounter)과 같은
     /// CounterViewModel 인스턴스를 공유한다. (데이터 교환 시나리오)
     ///
-    /// 주의: 이 화면은 비하인드 방식이라 PropertyChanged를 구독하지 않는다.
-    /// 따라서 공유 데이터가 "다른 화면(MVVM)에서" 바뀌면 이 화면은 그 변화를 알지 못해
-    /// 표시가 갱신되지 않는다(stale). 이것이 MVVM ↔ 순수 비하인드 데이터 교환의 문제다.
+    /// 비하인드 방식이지만 공유 ViewModel의 PropertyChanged를 추가로 구독하므로,
+    /// 다른 화면(MVVM)에서 데이터가 바뀌어도 이 화면이 함께 갱신된다.
+    /// (구독하지 않으면 MVVM에서 바뀐 값이 이 화면에 반영되지 않는 stale 문제가 생긴다)
     /// </summary>
     public partial class ucContent : UserControl
     {
@@ -23,13 +24,25 @@ namespace TestApp.Controls
         }
 
         /// <summary>
-        /// MVVM 화면과 공유할 ViewModel을 주입받는다. (단일 진실 공급원)
+        /// MVVM 화면과 공유할 ViewModel을 주입받고, 변경 알림을 구독한다. (단일 진실 공급원)
         /// </summary>
         public void AttachCounter(CounterViewModel vm)
         {
             _vm = vm;
+            //_vm.PropertyChanged += Vm_PropertyChanged;
             UpdateDisplay();
         }
+
+        /// <summary>
+        /// 공유 데이터(Count)가 어디서 바뀌든(비하인드/MVVM) 이 화면을 따라 갱신한다.
+        /// </summary>
+        //private void Vm_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    if (e.PropertyName == nameof(CounterViewModel.Count))
+        //    {
+        //        UpdateDisplay();
+        //    }
+        //}
 
         private void BtnIncrease_Click(object sender, RoutedEventArgs e)
         {
